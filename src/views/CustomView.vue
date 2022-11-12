50b871e64f7d0df8b7a90cc63aba31f517dc7715
Ilyass <ilyass.haddoud@gmail.com>
2022-11-12 17:57:48 +0100

    First commit

---
+++ src/views/CustomView.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>Hello world!</h1>
+    <p>Custom view created by ILYASS UCHIHA</p>
+  </div>
+</template>
+<script>
+export default {
+  name: "CustomView",
+};
+</script>
+<style></style>
